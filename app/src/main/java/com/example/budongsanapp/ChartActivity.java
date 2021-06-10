89d00eaad084d35554731dc89ee3290f8f2f94ef
taewoooh88 <taewoooh88@gmail.com>
2021-06-10 17:28:59 +0900

    5555

--- app/src/main/java/com/example/budongsanapp/ChartActivity.java
+++ app/src/main/java/com/example/budongsanapp/ChartActivity.java
@@ -8,7 +8,7 @@ public class ChartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);lll
         setContentView(R.layout.activity_chart);
     }
 }
